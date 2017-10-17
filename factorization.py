9b9acc737b36ff4dce7f65e06d7ce5b16fbc369a
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-10-16 22:32:44 -0400

    Adding primality test and quadratic sieve

--- factorization.py
+++ factorization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import euclidean
+import euclidean as e
+import primality as p
 
 def pMinusOne(n):
     a = 2
@@ -9,3 +10,18 @@ def pMinusOne(n):
         gcd = euclidean.gcd(a-1, n)
         if gcd < n and gcd > 1:
             return gcd
+
+def quadratic_sieve(n, low, high, b):
+    sieve = [ i**2 - n for i in range(low, high+1) ]
+    powers = set()
+
+    for i in range(2, b+1):
+        if p.is_prime(i):
+            powers.add(i)
+
+    for pwr in powers:
+        for i, s in enumerate(sieve):
+            while s % pwr == 0:
+                s[i] //= pwr
+
+    return sieve

65671df32ac8be67d406b8b3769c1b2a937bb79c
Trevor Stevenson <trevor.stevenson@duke.edu>
2017-10-17 20:13:33 -0400

    Fixing syntax bug

--- primality.py
+++ primality.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def is_prime(n):
-    for i range(2, int(sqrt(n))+1):
+    for i in range(2, int(sqrt(n))+1):
         if n % i == 0:
             return False
     return True
